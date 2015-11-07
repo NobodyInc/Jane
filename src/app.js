b05638f417d315244f51761ec30e9eb988c0394a
Andrew J <aandjjam@gmail.com>
2015-11-07 23:11:24 +0000

    silly

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
-console.log('intialized');
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {i: 0};
+  }
+  componentDidMount() {
+    setInterval(() => this.setState({i: (this.state.i + 1) % 5}), 500);
+  }
+  render() {
+    return (<h1>{this.props.heading.slice(0, this.state.i)}</h1>);
+  }
+}
+
+ReactDOM.render(<App heading='Jane'/>, document.getElementById('app'));
+
